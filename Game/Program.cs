d86db762b87c073a20088a0297b3111104a9f5e0
Admin <Admin@DESKTOP-8URQBFL>
2025-10-22 19:54:18 +0500

    HW DONE

--- Game/Program.cs
+++ Game/Program.cs
@@ -11,7 +11,7 @@ namespace Game
 	{
 		static void Main(string[] args)
 		{
-		    string simbol = "-*-";
+		    string simbol = "--*--";
 			int x = 0, y = 0;
 			
 			Console.CursorVisible = false;
@@ -19,43 +19,39 @@ namespace Game
 			while (Console.ReadKey().Key != ConsoleKey.Escape)
 			{
 
-				switch (Console.ReadKey().Key)
-				{
-					case ConsoleKey.LeftArrow:
-						if (x > 0) x--; break;
-					case ConsoleKey.RightArrow:
-						x++; break;
-					case ConsoleKey.UpArrow:
-						if (y > 0) y--; break;
-					case ConsoleKey.DownArrow:
-						y++; break;
-				}
+				//switch (Console.ReadKey().Key)
+				//{
+				//	case ConsoleKey.LeftArrow:
+				//		if (x > 0) x--; break;
+				//	case ConsoleKey.RightArrow:
+				//		x++; break;
+				//	case ConsoleKey.UpArrow:
+				//		if (y > 0) y--; break;
+				//	case ConsoleKey.DownArrow:
+				//		y++; break;
+				//}
 
-				//if (Console.ReadKey().Key == ConsoleKey.LeftArrow || Console.ReadKey().Key == ConsoleKey.A)
-				//{ if (x > 0) x--; }
-				//else if (Console.ReadKey().Key == ConsoleKey.RightArrow || Console.ReadKey().Key == ConsoleKey.D)
-				//{ x++; }
-				//else if (Console.ReadKey().Key == ConsoleKey.UpArrow || Console.ReadKey().Key == ConsoleKey.W)
-				//{ if (y > 0) y--; }
-				//else if (Console.ReadKey().Key == ConsoleKey.DownArrow || Console.ReadKey().Key == ConsoleKey.S)
-				//{ y++; }
+				if (Console.ReadKey().Key == ConsoleKey.LeftArrow || Console.ReadKey().Key == ConsoleKey.A)
+				{ if (x > 0) x--; }
+				else if (Console.ReadKey().Key == ConsoleKey.RightArrow || Console.ReadKey().Key == ConsoleKey.D)
+				{ x++; }
+				else if (Console.ReadKey().Key == ConsoleKey.UpArrow || Console.ReadKey().Key == ConsoleKey.W)
+				{ if (y > 0) y--; }
+				else if (Console.ReadKey().Key == ConsoleKey.DownArrow || Console.ReadKey().Key == ConsoleKey.S)
+				{ y++; }
 
 				Write(simbol, x, y);
 			}
 		}
 		public static void Write(string simbol, int x, int y)
 		{
-			try
-			{
-				if (x >= 0 && y >= 0)
+			if (x >= 0 && y >= 0)
 				{
 					Console.Clear();
 					Console.SetCursorPosition(x, y);
 					Console.Write(simbol);
 				}
-			}
-			catch (Exception)
-			{}
 		}
+			
 	}
 }
